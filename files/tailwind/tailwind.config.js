a7b39747deb8d0b5490ebe40ee77d72abd06e6e9
Prodigius <prodigisoftwares@gmail.com>
2025-09-20 07:54:11 -0600

    Update glob pattern for static JavaScript files

--- files/tailwind/tailwind.config.js
+++ files/tailwind/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     "../$DJANGO_PROJECT_NAME/apps/**/templates/**/*.{html,py,js}",
     "../$DJANGO_PROJECT_NAME/**/templates/**/*.{html,py,js}",
-    "../$DJANGO_PROJECT_NAME/static/**/*.{js}",
+    "../$DJANGO_PROJECT_NAME/static/**/*.js",
   ],
   media: false,
 };

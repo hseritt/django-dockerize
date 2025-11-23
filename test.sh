5251e22710cbf8f86903715b809bd522d9327ef8
harlin <harlin@localhost>
2025-11-23 08:44:15 -0700

    Issue #testing-11232025-1 - Fix container project name bug

--- test.sh
+++ test.sh
@@ -11,9 +11,9 @@ cd myproject
 # ./test.sh &&
 cd ..
 ./dev-up.sh
-docker-compose down -v
+docker compose down -v
 ./prod-up.sh
-docker-compose -f docker-compose.prod.yml down -v
+docker compose -f docker-compose.prod.yml down -v
 deactivate
 cd ..
 # sudo rm -rf master-project
